430183e13f4628c9ccdd365494795099d04b5020
Алексей Воевода <avoevoda433@gmail.com>
2021-12-14 00:00:42 +0300

    Add files via upload

---
+++ Lab__10/Lab10ex2/Lab10ex2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fonColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK) richTextBox1.BackColor = colorDialog1.Color;
+        }
+    }
+}
